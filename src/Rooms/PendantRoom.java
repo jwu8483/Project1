35665582b6c44010ef0ab993b89840e21566369b
1 <jwu8483@bths.edu>
2018-11-05 11:28:22 -0500

    asd

--- src/Rooms/PendantRoom.java
+++ src/Rooms/PendantRoom.java
@@ -3,6 +3,20 @@ package Rooms;
 import Game.Runner;
 import People.Person;
 
+public class PendantRoom extends Room
+{
+    public PendantRoom(int x, int y) {
+        super(x, y);
 
-public class PendantRoom {
+    }
+    public void enterRoom(Person x) {
+
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You enter the room and the door shuts behind you. You feel a hand cover your mouth and you then black out.");
+        Runner.gameOff();
+    }
+
+
 }
